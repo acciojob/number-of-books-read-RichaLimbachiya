595db358361939cc7019ad7d75c95002ea0eb5c0
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-15 10:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
+function numberOfBooksRead(library) {
+  // Count books with readingStatus === true
+  return library.filter(book => book.readingStatus === true).length;
+}
